4d95ee145007a4f62a096019f075223fc60f6d54
Nudel420 <115109618+Nudel420@users.noreply.github.com>
2025-08-08 23:23:08 +0200

    edited neovim and new init.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
+local kanso = require("kanso-ink")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 local act = wezterm.action
@@ -23,76 +23,7 @@ config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_max_width = 32
-config.colors = {
-	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = "#0b0022",
-
-		-- The active tab is the one that has focus in the window
-		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = "#3C5A78",
-			-- The color of the text for the tab
-			fg_color = "#c0c0c0",
-
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = "Bold",
-
-			-- Specify whether you want "None", "Single" or "Double" underline for
-			-- label shown for this tab.
-			-- The default is "None"
-			underline = "None",
-
-			-- Specify whether you want the text to be italic (true) or not (false)
-			-- for this tab.  The default is false.
-			italic = false,
-			-- Specify whether you want the text to be rendered with strikethrough (true)
-			-- or not for this tab.  The default is false.
-			strikethrough = false,
-		},
-
-		-- Inactive tabs are the tabs that do not have focus
-		inactive_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over inactive tabs
-		inactive_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab_hover`.
-		},
-
-		-- The new tab button that let you create new tabs
-		new_tab = {
-			bg_color = "#2D2648",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over the new tab button
-		new_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab_hover`.
-		},
-	},
-}
+config.colors = kanso.colors
 
 -- make inactive pain be less visible
 config.inactive_pane_hsb = {
@@ -263,7 +194,7 @@ end
 --  adds wave icon when leader is active
 wezterm.on("update-right-status", function(window, _)
 	local SOLID_LEFT_ARROW = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#b7bdf8" } }
+	local ARROW_FOREGROUND = { Foreground = { Color = "#a0a0b0" } }
 	local prefix = ""
 
 	if window:leader_is_active() then
@@ -272,11 +203,11 @@ wezterm.on("update-right-status", function(window, _)
 	end
 
 	if window:active_tab():tab_id() ~= 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#b7bdf8" } }
+		ARROW_FOREGROUND = { Foreground = { Color = "#a0a0b0" } }
 	end -- arrow color based on if tab is first pane
 
 	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#3b3052" } },
+		{ Background = { Color = "#595959" } },
 		{ Text = prefix },
 		ARROW_FOREGROUND,
 		{ Text = SOLID_LEFT_ARROW },
